e07e9b404b201f0eb7cdaad18b27e77ab3c331dd
Shashank Kumar <shashankkumar.cse.iitb@gmail.com>
2021-02-10 20:56:32 +0530

    Update evaluate.py

--- Assignment1/evaluate.py
+++ Assignment1/evaluate.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import confusion_matrix
+import numpy as np
 
 predicted = []
 actual = []
@@ -8,9 +9,27 @@ with open('evaluate.txt') as file:
         p, a = line.split('\n')[0].split(', ')
         predicted.append(int(p))
         actual.append(int(a))
-        line = file.readline()
 
 # Train => loss: 0.1772 - accuracy: 0.9510 - precision: 0.9679 - recall: 0.9321
 # Test =>  loss: 0.8543 - accuracy: 0.7356 - precision: 0.7687 - recall: 0.7120
 # Confusion matrix => true label along row and predicted label along column
-print(confusion_matrix(actual, predicted))
+matrix = confusion_matrix(actual, predicted)
+print('Insights into validation data')
+print(matrix)
+
+for i in range(5):
+    print(f'Class {i + 1} => Total entries = {matrix[i].sum()}, Accuracy = {round(matrix[i, i] / matrix[i].sum(), 2)}')
+
+print('Insights into predicted test labels')
+test = {}
+with open('predict.txt') as file:
+    lines = file.readlines()
+    for line in lines:
+        p = line.split('\n')[0]
+        if p in test.keys():
+            test[p] += 1
+        else:
+            test[p] = 1
+
+for key in test.keys():
+    print(f'Class {key} => Total predicted = {test[key]}')
