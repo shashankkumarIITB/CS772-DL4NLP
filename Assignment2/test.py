eb1d4c431fb5f66d3a1226a7c9e0f48d044970f4
the-emperior <amenaria2112@gmail.com>
2021-03-02 15:19:26 +0530

    refined the file

--- Assignment2/test.py
+++ Assignment2/test.py
@@ -4,7 +4,7 @@ import numpy as np
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 from neuralnet import NeuralNet
-from preprocess import preprocess_data
+from preprocess import preprocess_data, get_test_data, get_test_ratings
 
 # custom reviews to test the model
 # reviews = [
@@ -14,61 +14,37 @@ from preprocess import preprocess_data
 #     "Solid construction, good fit and finish.  The razor side just fits my razor.",
 # ]
 
-test_data = []
-test_ratings = []
-with open('gold_test.csv') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',')
-    for row in reader:
-        test_data.append(row['reviews'])
-        ratings = int(row['ratings'])
-        # Convert output ratings to one-hot encoding 
-        test_ratings.append(list(np.eye(5)[ratings-1]))
-
+test_data = get_test_data('gold_test.csv')
+test_ratings = get_test_ratings('gold_test.csv')
 
 # load the saved model
-nn_relu = NeuralNet.load_nn()
-# nn_sigmoid = NeuralNet.load_nn_sigmoid()
+nn = NeuralNet.load_nn()
 # preprocess the data
 reviews_processed = preprocess_data(test_data)
 # get the predictions from the model
-predictions_softmax_relu = nn_relu.predict(reviews_processed)
-# predictions_softmax_sigmoid = nn_sigmoid.predict(reviews_processed)
+predictions_softmax = nn.predict(reviews_processed)
 # convert the softmax predictions into class labels
-predictions_relu = predictions_softmax_relu.argmax(axis=1) + 1
-# predictions_sigmoid = predictions_softmax_sigmoid.argmax(axis=1) + 1
+predictions = predictions_softmax.argmax(axis=1) + 1
 
 # test_file = open("test_predict_sigmoid.txt","w+")
-
 # for p in predictions:
 #     test_file.write(f'{prediction}\n')
-
 # test_file.close()
 
-test_size = len(predictions_relu)
+test_size = len(predictions)
 match_count = 0
 
 for i in range(test_size):
-	p = predictions_relu[i]
+	p = predictions[i]
 	t = test_ratings[i]
 
 	if t[p-1]==1.0:
 		match_count += 1
 
-relu_acc = match_count*100/test_size
+acc = match_count*100/test_size
 
-# match_count = 0
+print('> test accuracy with relu+sigmoid activation = %.3f' % (acc))
 
-# for i in range(test_size):
-# 	p = predictions_sigmoid[i]
-# 	t = test_ratings[i]
-
-# 	if t[p-1]==1.0:
-# 		match_count += 1
-
-# sigmoid_acc = match_count*100/test_size
-
-print('> test accuracy with relu+sigmoid activation = %.3f' % (relu_acc))
-# print('> test accuracy with sigmoid activation = %.3f' % (sigmoid_acc))
 # print the results
 # for review, prediction in zip(reviews, predictions):
 #     print(f'{review} => {prediction}')
