610b717c7318a8eecc9e0a1f535e1e479a19468c
the-emperior <amenaria2112@gmail.com>
2021-03-02 16:23:43 +0530

    trained using normalized train dataset

--- Assignment2/train.py
+++ Assignment2/train.py
@@ -17,7 +17,7 @@ def main(train_file, test_file, load_model=False):
         return model.predict(test_reviews)
     else:
         # Hyperparameters
-        batch_size, epochs = 4, 3
+        batch_size, epochs = 1, 3
         # Read data from the training file and split the input and output
         train_data, train_ratings = get_train_data(train_file)
         # Preprocess the training reviews
@@ -31,7 +31,7 @@ def main(train_file, test_file, load_model=False):
 
 # Main function to run code from command line
 if __name__ == '__main__':
-    predictions_softmax = main('train.csv', 'test.csv', load_model=False)
+    predictions_softmax = main('train_balanced.csv', 'test.csv', load_model=False)
     predictions = predictions_softmax.argmax(axis=1) + 1
 
     # Write the predictions to a file
